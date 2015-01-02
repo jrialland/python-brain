ea1d25875d91438e961a267dccd2de9b102cefc5
jrialland <julien.rialland@gmail.com>
2015-01-02 16:54:55 +0100

    fixed pkg name for pypi

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
 	author_email='julien.rialland@gmail.com',
 	url='http://github.com/jrialland/python-brain',
 	download_url = 'https://github.com/jrialland/python-brain/tarball/0.1',
-        packages='brain',
+        packages=['brain'],
 	description='lightweight neural network library for Python',
 	provides=['brain'],
 	long_description='lightweight neural network library for Python',
