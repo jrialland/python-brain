261fb36ffe232beb540ae0f622a21bdc1fa1e2cf
jrialland <julien.rialland@gmail.com>
2015-01-02 17:01:53 +0100

    improved version

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python
 from setuptools import setup
+import brain
 
 setup(
 	name='brain',
-	version='0.1',
-	author='Julien Rialland',
-	author_email='julien.rialland@gmail.com',
+	version=brain.__version__,
+	author=brain.__author__,
+	author_email=brain.__email__,
 	url='http://github.com/jrialland/python-brain',
-	download_url = 'https://github.com/jrialland/python-brain/tarball/0.1',
+	download_url = 'https://github.com/jrialland/python-brain/tarball/'+str(brain.__version__),
         packages=['brain'],
 	description='lightweight neural network library for Python',
 	provides=['brain'],
 	long_description='lightweight neural network library for Python',
 	zip_safe=True,
-	license='BSD',
+	license=brain.__license__,
 	include_package_data=True,
 	keywords=['ia','neural network'],
 	classifiers=[
