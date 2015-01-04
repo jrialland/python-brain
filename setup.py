ef0581b6ed8dc8a39b460168ce1423915833f87b
jrialland <julien.rialland@gmail.com>
2015-01-04 11:59:41 +0100

    autopep8

--- setup.py
+++ setup.py
@@ -3,24 +3,25 @@ from setuptools import setup
 import brain
 
 setup(
-	name='pythonbrain',
-	version=brain.__version__,
-	author=brain.__author__,
-	author_email=brain.__email__,
-	url='http://github.com/jrialland/python-brain',
-	download_url = 'https://github.com/jrialland/python-brain/tarball/'+str(brain.__version__),
+    name='pythonbrain',
+        version=brain.__version__,
+        author=brain.__author__,
+        author_email=brain.__email__,
+        url='http://github.com/jrialland/python-brain',
+        download_url='https://github.com/jrialland/python-brain/tarball/' +
+            str(brain.__version__),
         packages=['brain'],
-	description='lightweight neural network library for Python',
-	provides=['brain'],
-	long_description='lightweight neural network library for Python',
-	zip_safe=True,
-	license=brain.__license__,
-	include_package_data=True,
-	keywords=['ia','neural network'],
-	classifiers=[
-		'Intended Audience :: Developers',
-		'Operating System :: OS Independent',
-		'Topic :: Software Development',
-		'Programming Language :: Python',
-	]
+        description='lightweight neural network library for Python',
+        provides=['brain'],
+        long_description='lightweight neural network library for Python',
+        zip_safe=True,
+        license=brain.__license__,
+        include_package_data=True,
+        keywords=['ia', 'neural network'],
+        classifiers=[
+                'Intended Audience :: Developers',
+                'Operating System :: OS Independent',
+                'Topic :: Software Development',
+                'Programming Language :: Python',
+        ]
 )
